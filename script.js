ad468608f0ff04b432918c322de8729e946d52ee
Swarnanjali-Chinthala <134590263+Swarnanjali-Chinthala@users.noreply.github.com>
2024-07-27 16:03:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -302,7 +302,7 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             const minutes = Math.floor(remainingTime / 1000 / 60);
             const seconds = Math.floor((remainingTime / 1000) % 60);
-            timerElement.textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+            timerElement.textContent = `Time Left : ${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
         }
     }
 
